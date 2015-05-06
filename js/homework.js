bad1dc3f149e7ddfc2203c9e0d252c31549db4ba
MacBook <MacBook@MacBooks-MacBook-Pro.local>
2015-05-06 09:50:04 +0100

    As far as i got...

---
+++ js/homework.js
@@ -0,0 +1,31 @@
+// function getValueonclick(message){
+//   return keyentered(message);
+// }
+
+// var click = document.getElementById('clicker');
+
+// click.addEventListener('click', function(){
+//   alert('I have been clicked!');
+ 
+
+var clickval = document.getElementsByClassName("number");
+  
+  clickval.addEventListener('click', function(){
+  alert('I have been clicked!');
+
+// clickNumber.addEventListener('click', function(event){
+//   alert('I have been clicked!');
+
+}
+
+);
+
+// var clickNumber = document.getElementsByTagName('ul');
+
+// clickNumber[0].addEventListener('click', function(event){
+
+// alert(event.target.innerHTML);
+// alert('I have been clicked!');
+// console.log(clickNumber[0]);
+
+// });
